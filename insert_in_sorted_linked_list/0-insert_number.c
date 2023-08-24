0234260fa344690338683cf24fb49680fbe410f5
WololoRC <wololoRC@proton.me>
2023-08-24 15:12:14 -0300

    start. duble. end

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -11,25 +11,48 @@ listint_t *insert_node(listint_t **head, int number)
     listint_t *new;
     listint_t *current;
 
-    current = *head;
-
     new = malloc(sizeof(listint_t));
     if (!new)
         return (NULL);
 
     new->n = number;
 
+    if (!*head) {
+        *head = new;
+        return (*head);
+    }
+
+    current = *head;
+
     while (current)
     {
-        if (current->n < new->n && current->next->n > new->n) {
+        if (current->n > new->n)
+        {
+           new->next = current;
+           *head = new;
+           return (new);
+
+        }
+        else if (new->n == current->n)
+        {
             new->next = current->next;
             current->next = new;
             return (new);
-        } else {
-            current = current->next;
+        }
+        else if (current->n < new->n && current->next == NULL)
+        {
+            current->next = new;
+            return (new);
+        }
+        else if (current->n < new->n && current->next->n > new->n)
+        {
+            new->next = current->next;
+            current->next = new;
+            return (new);
         }
 
-    }
+        current = current->next;
 
+    }
     return (NULL);
 }
