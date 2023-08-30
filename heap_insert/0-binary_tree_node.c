e39f7bed1dab9d8dbac68af3c50f23100694f957
WololoRC <wololoRC@proton.me>
2023-08-30 16:46:16 -0300

    initialized memory fix

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,16 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Insert a node on success else NULL
+ * binary_tree_node - Insert a on a binary tree.
  *
  *@parent: parent node
  *@value: value of new node
+ * Return: new node on succes, else NULL
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new = malloc(sizeof(binary_tree_t));
     if (!new)
         return (NULL);
+
+    new->parent = NULL;
+    new->left = NULL;
+    new->right = NULL;
 
     new->n = value;
 
