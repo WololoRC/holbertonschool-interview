fea33c432c8399f6a2595803d933a3c7514f8337
WololoRC <wololoRC@proton.me>
2023-09-07 12:40:25 -0300

    a

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""Minimum operatios"""
 
 if __name__ != "__main__":
     def divisors(n):
+        """Get a list of divisors"""
+
         return [item for item in range(2, n) if n % item == 0]
 
     def minOperations(n):
         """
         Calulates number of operations to reach 'n'
         """
+
         try:
             cnt = divisors(n)[1]
         except IndexError:
