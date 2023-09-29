6ae8a2e1352b215f5c159370516fb73a642529ec
WololoRC <wololoRC@proton.me>
2023-09-29 16:39:20 -0300

    aaa

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Wololo, Ayolo wololo! """
-from sys import stdin
+import sys
 import re
 
 cnt = 0
@@ -8,44 +8,47 @@ file_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 ocurrences = {}
 
-for line in stdin:
-    try:
-        l_line = re.split(
-            r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[([^\]]+)\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)$',
-            line
-        )
+try:
+    for line in sys.stdin:
+        try:
+            l_line = re.split(
+                r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[([^\]]+)\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)$',
+                line
+            )
 
-        l_line.pop(0)
-        l_line.pop()
+            l_line.pop(0)
+            l_line.pop()
 
-        file_size += int(l_line[3])
+            file_size += int(l_line[3])
 
-        x = status_codes.get(int(l_line[2]))
-        status_codes.update({int(l_line[2]): x + 1})
-        ocurrences.update({int(l_line[2]): status_codes.get(int(l_line[2]))})
+            x = status_codes.get(int(l_line[2]))
+            status_codes.update({int(l_line[2]): x + 1})
+            ocurrences.update({int(l_line[2]): status_codes.get(int(l_line[2]))})
 
-        cnt += 1
+            cnt += 1
 
-        if cnt == 10:
+            if cnt == 10:
+                return_list = []
+                cnt = 0
+                print("Total file size: {}".format(file_size))
+                for key in sorted(ocurrences.keys()):
+                    value = ocurrences[key]
+                    return_list.append("{}: {}".format(key, value))
+
+                for item in return_list:
+                    print(item)
+
+        except KeyboardInterrupt:
             return_list = []
-            cnt = 0
-            print("File size: {}".format(file_size))
-            for key, value in ocurrences.items():
+
+            print("Total file size: {}".format(file_size))
+            for key in sorted(ocurrences.keys()):
+                value = ocurrences[key]
                 return_list.append("{}: {}".format(key, value))
 
-            return_list.sort()
             for item in return_list:
                 print(item)
 
-    except KeyboardInterrupt:
-        return_list = []
-
-        print("File size: {}".format(file_size))
-        for key, value in ocurrences.items():
-            return_list.append("{}: {}".format(key, value))
-        
-        return_list.sort()
-        for item in return_list:
-            print(item)
-
+except KeyboardInterrupt:
+    pass
 
